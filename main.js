0aabc2965be6f2d0151e6b4bbf24b58a30d0257a
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-25 11:50:26 +0200

    added grid-change functionality (needs to maintain size, unfinished)

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 const container = document.querySelector('.container');
 
+let size = 16;
 
-for (i = 0; i < 16; i++) {
-    let column = document.createElement('div');
-    column.classList.add('column');
-    container.appendChild(column);
+renderColumns();
 
-    for (k = 0; k < 16; k++)
-    {
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('grid');
-        column.appendChild(newDiv);
+const button = document.querySelector('#button');
+
+button.addEventListener('click', () => {
+    size = parseInt(prompt('Choose a grid size (max: 100)'))
+    if (size > 100 || size == 'undefined' || size == 'NaN') {
+        size = 16;
     }
+    renderColumns();
+});
 
+function selectDivs() {
+    let divs = document.querySelectorAll('.grid');
+
+    divs.forEach((value) => {
+        value.addEventListener('mouseover', () => {
+            value.style.backgroundColor = 'blue';
+        })
+    });
 }
 
-let divs = document.querySelectorAll('.grid');
-
-divs.forEach((value) => {
-    value.addEventListener('mouseover', () => {
-        value.style.backgroundColor = 'blue';
-    })
-});
+function renderColumns() {
+    let columns = document.querySelectorAll('.column');
+    columns.forEach((value) => {
+        container.removeChild(value);
+    });
+    console.log('oi!');
+    for (i = 0; i < size; i++) {
+        let column = document.createElement('div');
+        column.classList.add('column');
+        container.appendChild(column);
+    
+        for (k = 0; k < size; k++)
+        {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('grid');
+            column.appendChild(newDiv);
+        }
+    }
+    selectDivs();
+}
